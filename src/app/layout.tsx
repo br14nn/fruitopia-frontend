293ecb3e5a5fa6704a339db64fbd843f053c81f0
br14nn <vituallabrian727@gmail.com>
2024-07-08 18:11:16 +0800

    setup & installations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-primary-100 min-h-screen w-full`}>
+        {children}
+      </body>
     </html>
   );
 }
