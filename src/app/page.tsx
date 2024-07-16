72ba1883980ebd0c0965d8d4e9f2cee3a39a8fed
br14nn <vituallabrian727@gmail.com>
2024-07-16 18:20:19 +0800

    mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,23 @@
 import Image from "next/image";
-import HeroBg from "@/assets/images/hero-bg.png";
+import HeroBg from "../assets/images/hero-bg.png";
 
 export default function Home() {
   return (
     <main className="flex w-full flex-col">
-      <section className="flex min-h-screen w-full flex-col items-center justify-center">
-        <Image
-          className="absolute inset-0 h-[100svh] w-full object-cover"
-          src={HeroBg}
-          alt="hero background"
-        />
-        <div className="absolute inset-0 h-[100svh] w-full bg-primary-default/30 bg-gradient-to-t from-primary-default" />
-        <div className="z-10 flex flex-col items-center justify-center gap-4 leading-none">
-          <h1 className="text-4xl font-bold tracking-widest text-secondary-default">
+      <section className="flex min-h-[50svh] w-full flex-col items-center justify-center bg-[url('../assets/images/hero-bg.png')] bg-cover bg-center lg:min-h-screen">
+        <div className="absolute inset-0 h-[50svh] w-full bg-primary-default/30 bg-gradient-to-t from-primary-default lg:min-h-screen" />
+        <div className="z-10 flex flex-col items-center justify-center gap-2 leading-none sm:gap-3 xl:gap-4">
+          <h1 className="text-lg font-bold tracking-widest text-secondary-default sm:text-2xl lg:text-3xl xl:text-4xl">
             FRUITOPIA
           </h1>
           <div className="flex w-full items-center justify-center gap-4">
-            <hr className="flex-grow rounded-full border-2 border-accent-default" />
-            <div className="aspect-square w-6 rotate-45 border-4 border-accent-default" />
-            <hr className="flex-grow rounded-full border-2 border-accent-default" />
+            <hr className="flex-grow rounded-full border border-accent-default xl:border-2" />
+            <div className="aspect-square w-4 rotate-45 border-2 border-accent-default xl:w-6 xl:border-4" />
+            <hr className="flex-grow rounded-full border border-accent-default xl:border-2" />
           </div>
-          <p className="mt-4 text-lg text-secondary-default/70">Est. 2024</p>
+          <p className="mt-1 text-secondary-default/70 sm:mt-2 sm:text-md xl:mt-4 xl:text-lg">
+            Est. 2024
+          </p>
         </div>
       </section>
       <section className="min-h-screen w-full"></section>
