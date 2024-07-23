4d65a12830fb7e65bc40ec580350b08e56c226d1
br14nn <vituallabrian727@gmail.com>
2024-07-23 14:24:14 +0800

    added new images

---
+++ src/utils/images.ts
@@ -0,0 +1,6 @@
+import SpringFruitImg from "@/assets/images/spring-fruit-thumbnail-min.jpg";
+import SummerFruitImg from "@/assets/images/summer-fruit-thumbnail-min.jpg";
+import FallFruitImg from "@/assets/images/fall-fruit-thumbnail-min.jpg";
+import WinterFruitImg from "@/assets/images/winter-fruit-thumbnail-min.jpg";
+
+export { SpringFruitImg, SummerFruitImg, FallFruitImg, WinterFruitImg };
