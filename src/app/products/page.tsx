6c2af79204191886255e1b71fdd7cdcfa74083f4
br14nn <vituallabrian727@gmail.com>
2024-08-09 22:31:51 +0800

    updated classNames

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,10 +3,12 @@ import FilterIcon from "@/components/svgs/FilterIcon";
 
 const ProductsPage = () => {
   return (
-    <main className="flex min-h-screen w-full flex-col gap-4 px-4">
-      <h1 className="mt-20 text-md font-bold tracking-widest">PRODUCTS</h1>
+    <main className="mx-auto flex min-h-screen w-full max-w-[1800px] flex-col gap-4 px-4 sm:px-16 lg:px-24">
+      <h1 className="mt-20 text-md font-bold tracking-widest sm:text-lg">
+        PRODUCTS
+      </h1>
 
-      <div className="flex w-full items-center gap-2">
+      <div className="flex w-full items-center gap-2 sm:w-[500px]">
         <SearchBar className="w-auto flex-grow" />
 
         <button className="h-10 w-10 rounded-md bg-secondary-default p-2 transition-colors duration-200 hover:bg-secondary-200">
