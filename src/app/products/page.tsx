734a88774f2ae606c5be3f4f576b09ab932530b9
br14nn <vituallabrian727@gmail.com>
2024-08-23 18:26:04 +0800

    added FilterButton component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import FilterButton from "@/components/FilterButton";
 import SearchBar from "@/components/SearchBar";
 import FilterIcon from "@/components/svgs/FilterIcon";
 import Text from "@/components/ui/Text";
@@ -12,9 +15,7 @@ const ProductsPage = () => {
       <div className="flex w-full items-center gap-2 sm:w-[500px]">
         <SearchBar className="w-auto flex-grow" />
 
-        <button className="h-10 w-10 rounded-md bg-secondary-default p-2 transition-colors duration-200 hover:bg-secondary-200">
-          <FilterIcon />
-        </button>
+        <FilterButton />
       </div>
     </main>
   );
